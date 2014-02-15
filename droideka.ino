609436f5d69e44188ba74b2c2bc8ad72be65db74
Derek Redfern <derek@quanttus.com>
2014-02-15 09:58:36 -0500

    Base file

---
+++ droideka.ino
@@ -0,0 +1,15 @@
+/*
+ * HackMIT Droideka
+ * 2/15/14
+ * Forrest Bourke, Derek Redfern, Eric Schneider, Paul Titchener
+ */
+
+// run once
+void setup() {              
+
+}
+
+// run ad nauseum
+void loop() {
+  
+}
